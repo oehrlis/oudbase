11476a53507a5b9c6cc85bee27c2560c5774aee8
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-05-24 10:31:33 +0200

    add instance check before start

--- local/oudbase/bin/oud_start_stop.sh
+++ local/oudbase/bin/oud_start_stop.sh
@@ -24,6 +24,7 @@ export OPENDS_JAVA_ARGS=-Dcom.sun.jndi.ldap.object.disableEndpointIdentification
 VERSION=v1.5.9
 DOAPPEND="TRUE"                                 # enable log file append
 VERBOSE="FALSE"                                 # enable verbose mode
+FORCE="FALSE"                                   # enable force restart
 SCRIPT_NAME=$(basename $0)
 START_HEADER="START: Start of ${SCRIPT_NAME} (Version ${VERSION}) with $*"
 ERROR=0
@@ -39,6 +40,7 @@ function Usage() {
     DoMsg "INFO : Usage, ${SCRIPT_NAME} [-hv -a <START|STOP> -i <OUD_INSTANCES>] [<START|STOP> <OUD_INSTANCES>]"
     DoMsg "INFO :   -h                 Usage (this message"
     DoMsg "INFO :   -v                 enable verbose mode"
+    DoMsg "INFO :   -f                 force startup will cause a restart if instance is running"
     DoMsg "INFO :   -a <start|stop>    Activity either start or stop"
     DoMsg "INFO :   -i <OUD_INSTANCES> List of OUD instances (default all market with Y in oudtab)"
     DoMsg "INFO : Logfile : ${LOGFILE}"
@@ -114,7 +116,7 @@ function CleanAndQuit() {
         44) DoMsg "ERR  : Exit Code ${1}. Can not create directory ${2}";;
         45) DoMsg "ERR  : Exit Code ${1}. Directory ${2} is not writeable";;
         50) DoMsg "ERR  : Exit Code ${1}. Error while performing exports";;
-        51) DoMsg "ERR  : Exit Code ${1}. Error while performing backups";;
+        51) DoMsg "ERR  : Exit Code ${1}. Error while starting instances";;
         52) DoMsg "ERR  : Exit Code ${1}. Error unknown activity ${2}";;
         99) DoMsg "INFO : Just wanna say hallo.";;
         ?)  DoMsg "ERR  : Exit Code ${1}. Unknown Error.";;
@@ -137,10 +139,12 @@ fi
 if [ "${OUD_BASE}" = "" ] || [ ! -d "${OUD_BASE}" ]; then
     CleanAndQuit 10
 fi
- 
-OUDENV=$(find $OUD_BASE -name oudenv.sh)
+
 # Load OUD environment
+OUDENV=$(find $OUD_BASE -name oudenv.sh)
 . "${OUDENV}" SILENT
+# get oud_status.sh script
+OUDSTATUS=$(find $OUD_BASE -name oud_status.sh)
  
 # Define Logfile
 LOGFILE="${LOG_BASE}/$(basename ${SCRIPT_NAME} .sh).log"
@@ -165,10 +169,11 @@ fi
 
 # usage and getopts
 DoMsg "INFO : processing commandline parameter"
-while getopts hva:i:E: arg; do
+while getopts hvfa:i:E: arg; do
     case $arg in
         h) Usage 0;;
         v) VERBOSE="TRUE";;
+        f) FORCE="TRUE";;
         a) MyActivity="${OPTARG}";;
         i) MyOUD_INSTANCES=$(echo "${OPTARG}"|sed s/\,/\ /g);;
         E) CleanAndQuit "${OPTARG}";;
@@ -237,24 +242,38 @@ for oud_inst in ${OUD_INST_LIST}; do
         continue
     fi
     INSTANCE_LOGFILE="${LOG_BASE}/$(basename ${SCRIPT_NAME} .sh)_${oud_inst}.log"
+    DoMsg "INFO : Initiate $MyActivity for ${DIRECTORY_TYPE} instance ${oud_inst}"
+    DoMsg "INFO : Check the progress in instance logfile ${INSTANCE_LOGFILE}"
     if [ "$MyActivity" == "START" ]; then
-        # check directory type
-        if [ ${DIRECTORY_TYPE} == "OUD" ]; then
-            DoMsg "INFO : Initiate $MyActivity for ${DIRECTORY_TYPE} instance ${oud_inst}"
-            $OUD_INSTANCE_HOME/OUD/bin/start-ds >> ${INSTANCE_LOGFILE} 2>&1 
-        elif [ ${DIRECTORY_TYPE} == "OUDSM" ]; then
-            DoMsg "INFO : Initiate $MyActivity for ${DIRECTORY_TYPE} instance ${oud_inst}"
-            $OUD_INSTANCE_HOME/startWebLogic.sh >> ${INSTANCE_LOGFILE} 2>&1 &
+        # check if instance is running
+        ${OUDSTATUS} -i $oud_inst 2>&1 >/dev/null
+        OUDSTATUS_ERROR=$?
+        if [ ${OUDSTATUS_ERROR} -eq 0 ] && [ ${FORCE} == "TRUE" ]; then
+            DoMsg "WARN : Force not yet implemented"
+        elif [ ${OUDSTATUS_ERROR} -eq 0 ] && [ ${FORCE} == "FALSE" ]; then
+            DoMsg "WARN : Instance $oud_inst already running"
+            ERROR=$((ERROR+1))
         else
-            DoMsg "WARN : [$oud_inst] Instance $oud_inst is not of type OUD/OUDSM. Skip $MyActivity for this instance."
-        fi 
+            # check directory type
+            if [ ${DIRECTORY_TYPE} == "OUD" ]; then
+                $OUD_INSTANCE_HOME/OUD/bin/start-ds >> ${INSTANCE_LOGFILE} 2>&1 
+            elif [ ${DIRECTORY_TYPE} == "OUDSM" ]; then
+                # clean up old WLS lok, DAT files
+                find $OUD_INSTANCE_HOME/servers -name "*.lok" -exec rm -f {} \; 2>/dev/null
+                find $OUD_INSTANCE_HOME/servers -name "*.DAT" -exec rm -f {} \; 2>/dev/null
+                nohup $OUD_INSTANCE_HOME/startWebLogic.sh >> ${INSTANCE_LOGFILE} 2>&1 &
+            else
+                DoMsg "WARN : [$oud_inst] Instance $oud_inst is not of type OUD/OUDSM. Skip $MyActivity for this instance."
+            fi 
+        fi
    elif  [ "$MyActivity" == "STOP" ]; then 
-          if [ ${DIRECTORY_TYPE} == "OUD" ]; then
-            DoMsg "INFO : Initiate $MyActivity for ${DIRECTORY_TYPE} instance ${oud_inst}"
+        if [ ${DIRECTORY_TYPE} == "OUD" ]; then
             $OUD_INSTANCE_HOME/OUD/bin/stop-ds >> ${INSTANCE_LOGFILE} 2>&1 
         elif [ ${DIRECTORY_TYPE} == "OUDSM" ]; then
-            DoMsg "INFO : Initiate $MyActivity for ${DIRECTORY_TYPE} instance ${oud_inst}"
-            $OUD_INSTANCE_HOME/bin/stopWebLogic.sh >> ${INSTANCE_LOGFILE} 2>&1 &
+            nohup $OUD_INSTANCE_HOME/bin/stopWebLogic.sh >> ${INSTANCE_LOGFILE} 2>&1 &
+            # clean up old WLS lok, DAT files
+            find $OUD_INSTANCE_HOME/servers -name "*.lok" -exec rm -f {} \;
+            find $OUD_INSTANCE_HOME/servers -name "*.DAT" -exec rm -f {} \;
         else
             DoMsg "WARN : [$oud_inst] Instance $oud_inst is not of type OUD/OUDSM. Skip $MyActivity for this instance."
         fi
