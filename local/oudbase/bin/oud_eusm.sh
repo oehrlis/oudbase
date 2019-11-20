89514cda12ce07ddfff6e811bf0a037ebaf60aca
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-11-20 16:12:55 +0100

    update build v1.7.2

--- local/oudbase/bin/oud_eusm.sh
+++ local/oudbase/bin/oud_eusm.sh
@@ -18,7 +18,7 @@
 # -----------------------------------------------------------------------
 
 # - Default Values ------------------------------------------------------
-VERSION=v1.7.1
+VERSION=v1.7.2
 JRE_HOME=$JAVA_HOME/jre/
 EUSMLIBDIR=$ORACLE_BASE/local/oudbase/lib
 ORACLEPKI=$ORACLE_HOME/oracle_common/modules/oracle.pki/oraclepki.jar
