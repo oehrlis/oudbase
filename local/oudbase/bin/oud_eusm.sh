851357e62b977954a4763bb3b6c0ca916834855d
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-11-20 16:11:51 +0100

    remove EUS java classes

--- local/oudbase/bin/oud_eusm.sh
+++ local/oudbase/bin/oud_eusm.sh
@@ -20,7 +20,7 @@
 # - Default Values ------------------------------------------------------
 VERSION=v1.7.1
 JRE_HOME=$JAVA_HOME/jre/
-EUSMLIBDIR=$ORACLE_BASE/local/eusm/lib
+EUSMLIBDIR=$ORACLE_BASE/local/oudbase/lib
 ORACLEPKI=$ORACLE_HOME/oracle_common/modules/oracle.pki/oraclepki.jar
 OSDT_CERT=$ORACLE_HOME/oracle_common/modules/oracle.osdt/osdt_cert.jar
 OSDT_CORE=$ORACLE_HOME/oracle_common/modules/oracle.osdt/osdt_core.jar
@@ -28,6 +28,14 @@ OJDBC8=$ORACLE_HOME/oracle_common/modules/oracle.jdbc/ojdbc8.jar
 RDBMSVER=19
 # - End of Default Values -----------------------------------------------
 
-$JRE_HOME/bin/java -classpath $ORACLEPKI:$OSDT_CERT:$OSDT_CORE:$JRE_HOME/lib/rt.jar:$OJDBC8:$EUSMLIBDIR/eusm.jar:$EUSMLIBDIR/ldapjclnt$RDBMSVER.jar oracle.security.eus.util.ESMdriver "$@"
+if [ -f "$ORACLE_BASE/local/oudbase/lib/eusm.jar" ] &&  [ -f "$ORACLE_BASE/local/oudbase/lib/ldapjclnt19.jar" ]; then
+    $JRE_HOME/bin/java -classpath $ORACLEPKI:$OSDT_CERT:$OSDT_CORE:$JRE_HOME/lib/rt.jar:$OJDBC8:$EUSMLIBDIR/eusm.jar:$EUSMLIBDIR/ldapjclnt$RDBMSVER.jar oracle.security.eus.util.ESMdriver "$@"
+else
+    echo "ERROR : Can not find $ORACLE_BASE/local/oudbase/lib/eusm.jar or $ORACLE_BASE/local/oudbase/lib/ldapjclnt19.jar"
+    echo "        Make sure to copy this two java classes from an Oracle 19c installation"
+    echo "        cp $ORACLE_HOME/rdbms/jlib/eusm.jar ."
+    echo "        cp $ORACLE_HOME/jlib/ldapjclnt19.jar ."
+fi
+
 
 # - EOF -----------------------------------------------------------------
