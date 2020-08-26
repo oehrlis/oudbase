cb8009ef41990fbaafe287e48c16a23cb22dc051
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-08-26 23:47:10 +0200

    fix tipo

--- local/oudbase/bin/oudbase_install.sh
+++ local/oudbase/bin/oudbase_install.sh
@@ -444,14 +444,17 @@ if [ "${SAVE_CONFIG}" = "TRUE" ]; then
 fi
 
 # remove new / empty core file
-if [ -f "${ETC_CORE}/${OUD_CORE_CONFIG}.new" ] && [ $(grep -v '^#' -c ${ETC_CORE}/${OUD_CORE_CONFIG}.new ) -eq 0 ]; then
-    rm "${ETC_CORE}/${OUD_CORE_CONFIG}.new"
+if [ -f "${ETC_CORE}/${OUD_CORE_CONFIG}.new" ];
+    if [ $(grep -v '^#' -c ${ETC_CORE}/${OUD_CORE_CONFIG}.new ) -eq 0 ]; then
+        rm "${ETC_CORE}/${OUD_CORE_CONFIG}.new"
+    fi
 fi
 
 # remove new / empty oudtab file
-${ETC_BASE}/$i.new
-if [ -f "${ETC_BASE}/oudtab.new" ] && [ $(grep -v '^#' -c ${ETC_BASE}/oudtab.new ) -eq 0 ]; then
-    rm "${ETC_BASE}/oudtab.new"
+if [ -f "${ETC_BASE}/oudtab.new" ]; then
+    if [ $(grep -v '^#' -c ${ETC_BASE}/oudtab.new ) -eq 0 ]; then
+        rm "${ETC_BASE}/oudtab.new"
+    fi
 fi
 
 # move config files
