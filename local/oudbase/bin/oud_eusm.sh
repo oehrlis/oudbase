5b5addfe5fb3ce5d46d1059b5540aa66e1050cf5
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-08-26 22:09:29 +0200

    build release 1.9.1

--- local/oudbase/bin/oud_eusm.sh
+++ local/oudbase/bin/oud_eusm.sh
@@ -18,7 +18,7 @@
 # -----------------------------------------------------------------------
 
 # - Default Values ------------------------------------------------------
-VERSION=v1.9.0
+VERSION=v1.9.1
 JRE_HOME=$JAVA_HOME/jre/
 EUSMLIBDIR=$ORACLE_BASE/local/oudbase/lib
 ORACLEPKI=$ORACLE_HOME/oracle_common/modules/oracle.pki/oraclepki.jar
