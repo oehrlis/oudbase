1031e39673d669248a014d4c596ae61d1fa0423d
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-02-23 07:38:58 +0100

    add pwgen to generate password and set default length to 15

--- local/oudbase/templates/create/oud12c_eus_ad_proxy/12_reset_eusadmin_password.sh
+++ local/oudbase/templates/create/oud12c_eus_ad_proxy/12_reset_eusadmin_password.sh
@@ -51,15 +51,19 @@ elif [ -n "${DEFAULT_PASSWORD}" ]; then
 else 
     # Auto generate a password
     echo "- auto generate new password..."
-    while true; do
-        s=$(cat /dev/urandom | tr -dc "A-Za-z0-9" | fold -w 10 | head -n 1)
-        if [[ ${#s} -ge 8 && "$s" == *[A-Z]* && "$s" == *[a-z]* && "$s" == *[0-9]*  ]]; then
-            echo "- passwort does Match the criteria"
-            break
-        else
-            echo "- password does not Match the criteria, re-generating..."
-        fi
-    done
+    if [ $(command -v pwgen) ]; then 
+        s=$(pwgen -s -1 15)
+    else 
+        while true; do
+            # use urandom to generate a random string
+            s=$(cat /dev/urandom | tr -dc "A-Za-z0-9" | fold -w 15 | head -n 1)
+            # check if the password meet the requirements
+            if [[ ${#s} -ge 10 && "$s" == *[A-Z]* && "$s" == *[a-z]* && "$s" == *[0-9]*  ]]; then
+                echo "$s"
+                break
+            fi
+        done
+    fi
     echo "- use auto generated password for user ${EUS_USER_NAME}"
     ADMIN_PASSWORD=$s
     echo "- save password for ${EUS_USER_NAME} in ${EUSADMIN_USERS_PWD_FILE}"
